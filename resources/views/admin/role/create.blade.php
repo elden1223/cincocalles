836e1c7712561ab3451981c00eea5bb67986c86c
Elden Gutierrez <elden.gmmail.com>
2022-09-29 00:12:26 -0500

    Change route

--- resources/views/admin/role/create.blade.php
+++ resources/views/admin/role/create.blade.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
         <h3>Rol: Crear</h3>
         <div class="col-md-12">
-            <form action="{{ secure_route('role.store') }}" method="post">
+            <form action="{{ route('role.store') }}" method="post">
                 @csrf
                 @include('admin.role.form')
                 <div class="mb-3">
                     <input class="btn btn-primary" type="submit" name="action" value="Registrar">
-                    <a class="btn btn-danger" href="{{ secure_route('roles') }}"> Cancelar</a>
+                    <a class="btn btn-danger" href="{{ route('roles') }}"> Cancelar</a>
                 </div>
             </form>
         </div>
